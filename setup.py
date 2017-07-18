ec309cda47c8c7ab8b3ae876083b1cbffc6cfdb4
Alan Pich <alanp@native.com>
2017-07-18 16:12:18 +0100

    updates for v1.2

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 from setuptools import setup
 
 setup(name='TMCL',
-      version='1.1.2',
-      description='Talk to Trinamic Stepper Motors using TMCL over serial',
-      url='https://github.com/NativeDesign/python-tmcl',
-      author='Alan Pich',
-      author_email='alanp@native.com',
-      license='MIT',
-      packages=['TMCL'],
-      install_requires=[
-        'pyserial'
-      ],
-      zip_safe=False)
+	version='1.2',
+	description='Talk to Trinamic Stepper Motors using TMCL over serial',
+	url='https://github.com/NativeDesign/python-tmcl',
+	author='Alan Pich',
+	author_email='alanp@native.com',
+	license='MIT',
+	packages=['TMCL'],
+	install_requires=[
+		'pyserial'
+	],
+	keywords = [
+		'tmcl',
+	    'trinamic',
+		'rs485',
+		'stepper',
+		'motor',
+		'tmcm'
+	],
+	zip_safe=False)
